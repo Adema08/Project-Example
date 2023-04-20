021d5a8874a7b50d86def47a745ab4c6364e19c9
Adema <aatygaeva08@gmail.com>
2023-04-20 23:37:26 +0600

    Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter 1st number");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 2nd number");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Sum:");
+Console.WriteLine(x + y);
