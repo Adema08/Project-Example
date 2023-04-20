6789bb85592f784653bdc33b6fd359426031b6e8
Adema <aatygaeva08@gmail.com>
2023-04-21 00:01:52 +0600

    Hello my sweety

---
+++ Example005_HelloMySweety/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+if (name.ToUpper() == "ADEMA")
+{
+    Console.WriteLine("Hello, my Sweety");
+}
+else
+{
+    Console.WriteLine("Go away!!!");
+}
