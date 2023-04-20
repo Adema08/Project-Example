476fd0217d08d3d6dcc642867b39807bca295a39
Adema <aatygaeva08@gmail.com>
2023-04-20 23:42:57 +0600

    Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter 1st number");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter 2nd number");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Div:");
+Console.WriteLine(x / y);
+
